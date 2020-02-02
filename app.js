96665cf13dfd226f8c1e6820ca14bda4dfc17c30
Antti Pessa <anttipessa@gmail.com>
2020-02-02 21:38:15 +0200

    messing around with cookies

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const socket = require('socket.io');
 const helmet = require("helmet");
+const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(helmet());
+app.use(cookieParser());
+
 require('./db')();
 
 const server = app.listen(3000, () => {
@@ -12,16 +15,22 @@ const server = app.listen(3000, () => {
 module.exports = server;
 app.use(express.static('public'))
 
+
+
 const io = socket(server)
-let num = 0;
+let num = 20;
 io.on('connection', (socket) => {
     console.log('made connection', socket.id)
     socket.on('press', (data) => {
-        num = num + 1
+        num = num - 1
         console.log(num, socket.id)
-        if(num == 100){
-            console.log(socket.id, 'WINNER')
+        if (num == 0) {
+            socket.emit('restart')
+            num = 20
+        } else {
+            socket.emit('points', num)
         }
+
     })
 })
 
--- public/btn.js
+++ public/btn.js
@@ -1,9 +1,21 @@
 const socket = io.connect('http://localhost:3000/')
 
 const btn = document.getElementById('btn')
+const points = document.getElementById('points');
 
 btn.addEventListener('click', () => {
  socket.emit('press',{
      num: 1
  });
 })
+
+socket.on('points', function(data){
+    points.innerHTML = '';
+    points.innerHTML += '<p><strong>' + data + '</strong></p>';
+});
+
+socket.on('restart', function(){
+    points.innerHTML = '';
+    points.innerHTML += '<p><strong> Time to restart? </strong></p>';
+    location.reload()
+});
