2f02c5149f585fa52f4f0f5c787c7263c565cf79
Antti Pessa <anttipessa@gmail.com>
2020-02-16 14:40:36 +0200

    added cookies

--- app.js
+++ app.js
@@ -7,26 +7,29 @@ const app = express();
 app.use(helmet());
 app.use(cookieParser());
 
-require('./db')();
+//require('./db')();
 
 const server = app.listen(3000, () => {
     console.log('Server running at localhost:3000')
 });
 module.exports = server;
+
 app.use(express.static('public'))
 
 
 
 const io = socket(server)
 let num = 20;
+let counter = 0;
 io.on('connection', (socket) => {
     console.log('made connection', socket.id)
     socket.on('press', (data) => {
         num = num - 1
+        counter = counter + 1;
         console.log(num, socket.id)
-        if (num == 0) {
-            socket.emit('restart')
-            num = 20
+        if (counter % 10==0) {
+            num = num + 5
+            socket.emit('restart', num)
         } else {
             socket.emit('points', num)
         }
--- public/btn.js
+++ public/btn.js
@@ -2,20 +2,40 @@ const socket = io.connect('http://localhost:3000/')
 
 const btn = document.getElementById('btn')
 const points = document.getElementById('points');
+const info = document.getElementById('info');
 
 btn.addEventListener('click', () => {
- socket.emit('press',{
-     num: 1
- });
+    socket.emit('press', {
+        num: 1
+    });
 })
 
-socket.on('points', function(data){
+socket.on('points', function (data) {
     points.innerHTML = '';
+    info.innerHTML = '';
+    points.innerHTML += '<p><strong>' + data + '</strong></p>';
+    setCookie("player", data, 3)
+});
+
+socket.on('restart', function (data) {
+    info.innerHTML = '';
+    points.innerHTML = '';
+    info.innerHTML += '<p><strong> You win 5 points</strong></p>';
     points.innerHTML += '<p><strong>' + data + '</strong></p>';
 });
 
-socket.on('restart', function(){
-    points.innerHTML = '';
-    points.innerHTML += '<p><strong> Time to restart? </strong></p>';
-    location.reload()
-});
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookieValue() {
+    var b = document.cookie.match('(^|[^;]+)\\s*' + "player" + '\\s*=\\s*([^;]+)');
+    b = b ? b.pop() : '';
+     points.innerHTML += '<p><strong>' + b + '</strong></p>'
+}
+
+
+
