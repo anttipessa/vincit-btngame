b36951619696db832b49aa52e09fe6c366449cf5
Antti Pessa <anttipessa@gmail.com>
2020-02-20 20:32:44 +0200

    localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,36 @@ import './App.css';
 class Btn extends Component {
 
   state = {
-    count: 20
+    count: localStorage.getItem("score") || 20
   };
 
   handleClick = () => {
     this.setState(({ count }) => ({
       count: count - 1
     }));
+    localStorage.setItem("score", this.state.count - 1)
+  };
+
+  reset = () => {
+    this.setState(() => ({
+      count: 20
+    }));
   };
 
   render() {
-    if( this.state.count === 0){
-      return (<Button size="large" variant="contained" color="secondary" disabled onClick={this.handleClick}>Click</Button>)
+    if (this.state.count <= 0) {
+      return (
+        <div>
+          <Button size="large" variant="contained" color="secondary" disabled onClick={this.handleClick}>Click Me</Button>
+          <br></br>
+          <Button size="large" variant="contained" color="primary" onClick={this.reset}>Try again?</Button>
+        </div>)
     }
-    return(
-    <div>
-      <div>{this.state.count}</div>
-      <Button size="large" variant="contained" color="secondary" onClick={this.handleClick}>Click</Button>
-    </div>
+    return (
+      <div>
+        <div>{this.state.count}</div>
+        <Button size="large" variant="contained" color="secondary" onClick={this.handleClick}>Click</Button>
+      </div>
     )
   }
 }
